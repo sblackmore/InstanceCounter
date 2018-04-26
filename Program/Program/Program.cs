f809d42c6bc9d8068da7ccb3099ec67a6359d73c
Stephen Blackmore <blackmore.stephen@gmail.com>
2018-04-26 09:45:20 -0400

    Added a try/catch/finally to handle exceptions when calling the garbage collector.

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -20,10 +20,20 @@
             Console.Out.WriteLine("\r\nSetting one of the CountableObject1 instances to null...");
             one = null;
             Console.Out.WriteLine("Requesting Garbage Collection...");
-            GC.Collect();
-            Console.Out.Write("Sleeping for 5 seconds...\r\n");
-            System.Threading.Thread.Sleep(5000);
-            factory.printStatistics();
+            try
+            {
+                GC.Collect();
+                Console.Out.Write("Sleeping for 5 seconds...\r\n");
+                System.Threading.Thread.Sleep(5000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                factory.printStatistics();
+            }
         }
     }
 }
